d44ff47fb56227c1f4c04f74dfc8b576db40de47
Dawid Holka <dawidholka97@gmail.com>
2020-12-19 21:55:39 +0100

    Update

--- js/TinyMCEField.vue
+++ js/TinyMCEField.vue
@@ -2,11 +2,32 @@
   <lit-base-field :field="field">
     <!-- ... -->
   </lit-base-field>
+  <editor
+      api-key="no-api-key"
+      :init="{
+         height: 500,
+         menubar: false,
+         plugins: [
+           'advlist autolink lists link image charmap print preview anchor',
+           'searchreplace visualblocks code fullscreen',
+           'insertdatetime media table paste code help wordcount'
+         ],
+         toolbar:
+           'undo redo | formatselect | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help'
+       }"
+  />
 </template>
 
 <script>
+import Editor from '@tinymce/tinymce-vue'
+
 export default {
   name: 'TinyMCEField',
+  components: {
+    'editor': Editor
+  },
   props: {
     field: {
       required: true,
--- js/litstack-tinymce.js
+++ js/litstack-tinymce.js
@@ -1,6 +1,7 @@
 import Lit from '../vendor/litstack/litstack/resources/js/common/lit';
 import TinyMCEField from "./TinyMCEField";
 
+
 Lit.booting((Vue) => {
     Vue.component('tinymce-field', TinyMCEField);
 });
