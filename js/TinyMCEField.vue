bbbe4568b8952b22e2f8c8471df91ac79ea2854f
Dawid Holka <dawidholka97@gmail.com>
2021-03-13 14:13:03 +0100

    feat: add source code editor

--- js/TinyMCEField.vue
+++ js/TinyMCEField.vue
@@ -12,7 +12,7 @@
            'preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr nonbreaking toc insertdatetime advlist lists wordcount imagetools textpattern noneditable charmap quickbars emoticons'
          ],
          toolbar:
-           'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | charmap emoticons | fullscreen  preview save | image media link codesample | ltr rtl',
+           'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | charmap emoticons | fullscreen  preview save | image media link codesample code | ltr rtl',
          quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
          noneditable_noneditable_class: 'mceNonEditable',
          toolbar_mode: 'sliding',
